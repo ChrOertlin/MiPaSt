5654babe8fe17bed6edf25a779e024926875b15e
bigcat <bigcat@bigcat-SN68PT.(none)>
2014-09-25 16:23:30 +0200

    Update

--- org.pathvisio.mipast/src/org/pathvisio/mipast/MiPaStPlugin.java
+++ org.pathvisio.mipast/src/org/pathvisio/mipast/MiPaStPlugin.java
@@ -1,6 +1,14 @@
 package org.pathvisio.mipast;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.awt.Desktop;
+import java.net.URI;
+
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -11,19 +19,86 @@ public class MiPaStPlugin implements BundleActivator, Plugin {
 	
 	private JMenu miPaStMenu;
 	private PvDesktop desktop;
+	private JMenuItem menuLoadFiles;
+	private JMenuItem help;
+	private JMenuItem documentation;
 	
 	@Override
 	public void init(PvDesktop desktop) {
 		this.desktop = desktop;
-		miPaStMenu = new JMenu("MiPaStMenu");
-		desktop.registerSubMenu("Plugins", miPaStMenu);
+		
+		
+		menuItems();
 	}
 
+	public void menuItems(){
+		miPaStMenu = new JMenu("MiPaStMenu");
+		desktop.registerSubMenu("Plugins", miPaStMenu);
+		
+		menuLoadFiles = new JMenuItem("Load dataset files");
+		//menuLoadFiles.addActionLister(new menuActionListener());
+		
+		help = new JMenuItem("Help");
+		help.addActionListener(new HelpActionListener());
+		
+		documentation = new JMenuItem("Documentation");
+		documentation.addActionListener(new DocumentationActionListener());
+		
+		miPaStMenu.add(menuLoadFiles);
+		miPaStMenu.add(help);
+		miPaStMenu.add(documentation);
+		
+	}
+
+	
 	@Override
 	public void done() {
 		desktop.unregisterSubMenu("Plugins", miPaStMenu);
 	}
 
+	//Opens URL to help page of the plugin
+	class HelpActionListener implements ActionListener{
+		
+		public void helpURL() throws Exception{
+		Desktop helpBrowse= Desktop.getDesktop();
+		helpBrowse.browse(new URI("https://www.google.com"));}
+		
+		public void actionPerformed(ActionEvent e){
+		try {
+			helpURL();
+		} catch (Exception e1) {
+			
+			e1.printStackTrace();
+		}
+		
+		
+		    
+		}
+		
+	}
+	
+	class DocumentationActionListener implements ActionListener{
+		
+		public void documentationURL() throws Exception{
+		Desktop documentationBrowse= Desktop.getDesktop();
+		documentationBrowse.browse(new URI("https://www.google.com"));}
+		
+		public void actionPerformed(ActionEvent e){
+		try {
+			documentationURL();
+		} catch (Exception e1) {
+			
+			e1.printStackTrace();
+		}
+		
+		
+		    
+		}
+		
+	}
+	
+	
+// Bundle Activator Starts the plugin withit PathVisio
 	@Override
 	public void start(BundleContext context) throws Exception {
 		context.registerService(Plugin.class.getName(), this, null);
